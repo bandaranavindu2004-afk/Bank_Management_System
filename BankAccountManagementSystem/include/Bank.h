e2ea54b1af1cbd86360fdd66c4579e0e09326507
R.N.KESHAN <en121320@foe.sjp.ac.lk>
2026-07-02 20:32:05 +0530

    bank header edited

---
+++ BankAccountManagementSystem/include/Bank.h
@@ -0,0 +1,24 @@
+#include <vector>
+#include "Account.h"
+#include "Customer.h"
+#include "Transaction.h"
+
+class Bank {
+private:
+    vector<Account*> accounts;
+    vector<Customer> customers;
+    vector<Transaction> transactions;
+    int nextTransactionID;
+
+public:
+    Bank();
+    void addCustomer(const Customer& customer);
+    void addAccount(Account* account);
+    Account* findAccount(int accountNumber);
+    void depositMoney(int accountNumber, double amount);
+    void withdrawMoney(int accountNumber, double amount);
+    void showAllCustomers() const;
+    void showAllAccounts() const;
+    void showAllTransactions() const;
+    ~Bank();
+};
