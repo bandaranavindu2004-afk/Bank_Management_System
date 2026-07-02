630e628150cf2e9aaf77d91680de278b905b1449
R.N.KESHAN <en121320@foe.sjp.ac.lk>
2026-07-02 20:17:30 +0530

    transaction date and time

--- BankAccountManagementSystem/include/Transaction.h
+++ BankAccountManagementSystem/include/Transaction.h
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
-struct date{
-    int day, month, year;
-}
 
 class transaction{
     private:
         int transID;
-        date transdate;
+        string transdate_time;
         string transtype;
         float transamount;
 
     public:
         transaction(string type, float amount):transtype(type), transamount(amount){}
 
+        void setCurrentDateTime(){};
+
         void showtrans() const {
             cout << "Transaction ID : " << transID << endl;
-            cout << "Transaction Date : " << transdate.day << "/" << transdate.month << "/" << transdate.year << endl;
+            cout << "Transaction Date & Time : " <<transdate_time<< endl;
             cout << "Transaction Type : " << transtype << endl;
             cout << "Transaction Amount : " << transamount << endl;
         }
 };
+
+
+void transaction::setCurrentDateTime() {
+        time_t now = time(0);
+        transdate_time = ctime(&now);
+    }
+}
