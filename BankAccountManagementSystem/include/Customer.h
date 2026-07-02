98287cdb93e76f355c7a797d619b370b7c032490
lightseeker204 <bandaranavindu2004@gmail.com>
2026-07-02 19:20:34 +0530

    Customer Header

---
+++ BankAccountManagementSystem/include/Customer.h
@@ -0,0 +1,25 @@
+#include <string>
+using namespace std;
+
+class Customer {
+    private:
+        string customerID;
+        string name;
+        string address;
+        int phonenum;
+    
+    public:
+        Customer(string customerID, string name, string address, int phonenum){
+            this->customerID=customerID;
+            this->name=name;
+            this->address=address;
+            this->phonenum=phonenum;
+        }
+
+        void getid(){
+            cout << "Customer ID : " << customerID << endl;
+            cout << "Customer Name : " << name << endl;
+            cout << "Customer Address : " << address << endl;
+            cout << "Customer Phone Number : " << phonenum << endl;
+        }
+};
